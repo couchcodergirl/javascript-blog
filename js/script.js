857165efee1745c14e5df4c084299d53f520cdcb
Emilia Piechocka <couchcodergirl@gmail.com>
2020-02-26 11:48:28 +0200

    Add js functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-'use strict'
+const titleClickHandler = function(event){
+  console.log(event);/* czy to dobrze ten event?*/
+
+  /* remove class 'active' from all article links  */
+   const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+    const activeLinks = document.querySelectorAll('.articles a.active');
+
+    for(let activeArticle of activeArticles){
+    activeLink.classList.remove('active');
+    }
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+
+function(event); /*jak wywołać funkcję?*/
