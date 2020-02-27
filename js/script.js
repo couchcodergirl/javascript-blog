5ef1d8205047f2f4ead84d316e9e100921eab06d
Emilia Piechocka <couchcodergirl@gmail.com>
2020-02-27 14:54:56 +0200

    Add some js const

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const titleClickHandler = function(event) {
     event.preventDefault();
 
@@ -35,3 +36,48 @@ const links = document.querySelectorAll('.titles a');
 for(const link of links) {
     link.addEventListener('click', titleClickHandler);
 }
+
+//TITLE LIST
+const opt = {
+    articleSelector: '.post',
+    articleTagsSelector: '.post-tags .list',
+    articleAuthorSelector: '.post-author',
+    titleSelector: '.post-title',
+    titleListSelector: '.titles',
+    tagsListSelector: '.tags.list',
+    cloudClassCount: 5,
+    cloudClassPrefix: 'tag-size-',
+    authorsListSelector: '.authors.list'
+  }
+//z kodilli
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* DONE remove contents of titleList */
+  const titleList = document.querySelector(opt.titleListSelector);
+
+    titleList.innerHTML = '';
+
+  /* for each article */
+  for (let article of articles) {
+      /*[DONE] get the article id */
+  const articleId = article.getAttribute('id');
+
+    /* get the article id */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* find the title element */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log(linkHTML)
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+    }
+}
+
+generateTitleLinks();
