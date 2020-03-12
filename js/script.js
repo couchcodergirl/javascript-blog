a3ac2a3883c59ff7a1d33e4d5a5983424b4ced8c
Emilia Piechocka <couchcodergirl@gmail.com>
2020-03-12 13:50:07 +0200

    Add alltags js

--- js/script.js
+++ js/script.js
@@ -97,10 +97,34 @@ function generateTitleLinks(customSelector = ''){
 
 generateTitleLinks();
 
+/*function calculateTagsParams(tags) {
+    const params = {
+      max: 0,
+      min: 999999
+    };
+*/
+
+function calculateTagsParams(tags) {
+    const params = {
+      max: 0,
+      min: 999999
+    };
+
+    for (let tag in tags) {
+      params.max = Math.max(tags[tag], params.max);
+      params.min = Math.min(tags[tag], params.min);
+    }
+    return params;
+    console.log(params);
+  }
+
+
 //GENERATE TAGS
 function generateTags(){
+    
   /* [NEW] create a new variable allTags with an empty array */
-  let allTags = [];
+  let allTags = {};
+  console.log(allTags);
     
   /* find all articles */
 
@@ -146,11 +170,13 @@ function generateTags(){
       console.log(html);
         
       /* [NEW] check if this link is NOT already in allTags */
-      if(allTags.indexOf(linkHTML) == -1){
-          
-      /* [NEW] add generated code to allTags array */
-        allTags.push(linkHTML);
-        }
+      if(!allTags[tag]) {          
+      /* [NEW] add tag to allTags object */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
+
 
       /* END LOOP: for each tag */
     }
@@ -166,15 +192,32 @@ function generateTags(){
     const tagList = document.querySelector('.tags');
 
     /* [NEW] add html from allTags to tagList */
-    tagList.innerHTML = allTags.join(' ');
+    //tagList.innerHTML = allTags.join(' ');
+    console.log(allTags);
+    
+    /* [NEW] create variable for all links HTML code */
+    const tagsParams = calculateTagsParams(allTags);
+    console.log('tagsParams:', tagsParams);
+    let allTagsHTML = '';
 
-  }
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+    
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
+    console.log(allTags);
+
 
   const tags = document.querySelectorAll('.post-tags .list li a');
 
   for(let tag of tags){
     tag.addEventListener('click', tagClickHandler);
   }
+}
 
 
 generateTags();
@@ -391,4 +434,3 @@ function addClickListenersToAuthors(){
 }
 
 addClickListenersToAuthors();
-
